6f3364ba0ba4e06082f12953a95f139b76de3af7
Mark Dietzer <git@doridian.net>
2022-04-29 11:08:14 -0700

    Add Crossposter

--- mastodon/mastodon-secrets.sh
+++ mastodon/mastodon-secrets.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 kubectl delete secret -n mastodon mastodon-env
 kubectl create secret generic -n mastodon mastodon-env --from-env-file=.\mastodon\mastodon-secrets.env
+
+kubectl delete secret -n mastodon mastodon-twitter-poster-env
+kubectl create secret generic -n mastodon mastodon-twitter-poster-env --from-env-file=.\mastodon\mastodon-twitter-poster-secrets.env
