b65b7173f3656d04a9da86cf803eaef166f67a42
Mark Dietzer <git@doridian.net>
2022-04-30 15:52:17 -0700

    upgrade install

--- helm.sh
+++ helm.sh
@@ -5,12 +5,12 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml
-helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.8.0
+helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.8.0
 
 helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --set controller.publishService.enabled=true --set controller.service.externalTrafficPolicy=Local --set-string controller.config.use-forward-headers=true,controller.config.compute-full-forward-for=true,controller.config.use-proxy-protocol=true --set controller.service.annotations."service\.beta\.kubernetes\.io/vultr-loadbalancer-proxy-protocol=true" --set controller.service.annotations."service\.beta\.kubernetes\.io/vultr-loadbalancer-private-network=true" --set controller.service.annotations."service\.beta\.kubernetes\.io/vultr-loadbalancer-algorithm=least_connections" --namespace ingress-nginx --create-namespace
 
 kubectl create namespace postgresql
-helm install postgres-operator postgres-operator-charts/postgres-operator --namespace=postgresql
-helm install postgres-operator-ui postgres-operator-ui-charts/postgres-operator-ui --namespace=postgresql
+helm upgrade --install postgres-operator postgres-operator-charts/postgres-operator --namespace=postgresql
+helm upgrade --install postgres-operator-ui postgres-operator-ui-charts/postgres-operator-ui --namespace=postgresql
 
-helm install redis-operator redis-operator/redis-operator --namespace=redis-operator --create-namespace
+helm upgrade --install redis-operator redis-operator/redis-operator --namespace=redis-operator --create-namespace
